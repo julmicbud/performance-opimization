0f1926b696b19562e71a89f17c0ae49319ce5c2c
Julia Hendrickson <julmicbud@gmail.com>
2016-03-22 15:02:23 -0400

    finished fixing fps i extracted all the varibles I thought could be done outside of the loops.

--- views/js/main.js
+++ views/js/main.js
@@ -511,9 +511,9 @@ function updatePositions() {
   // Distance to move pizzas
   var dist;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-		console.log("phase = " + phase);
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    phase = Math.sin(scrollTopCalc + (i % 5));
+    dist = items[i].basicleft + 100 * phase - 1024 + 'px';
+    items[i].style.transform = 'translateX'(' + dist + ')';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
@@ -533,7 +533,11 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  // The amount of rows depending on the browser height
+  var rows = Math.ceil(window.innerHeight / s);
+  //total pizza number
+  var totalPizzas = rows * cols;
+  for (var i = 0; i < totalPizzas; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
@@ -541,7 +545,8 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    //changed querySelector to getElementById more efficent
+    document.getElementById("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
 });
